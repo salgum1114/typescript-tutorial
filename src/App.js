dea5efc81e991eca18a5c19ad731043902583358
sgoh <sgoh@nkia.co.kr>
2019-01-23 18:04:57 +0900

    function test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { Button, Input } from '../lib';
 
+import { myFunc } from './utils/utils';
+
 class App extends React.Component {
     render() {
-        
+        const circle = {
+            size: 1,
+            radius: 2,
+        }
+        const rectangle = {
+            size: 1,
+            width: 5,
+        }
+        myFunc(circle);
+        myFunc(rectangle);
         return (
             <div>
                 <Button>test</Button>
